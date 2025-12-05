3821279df76dbd8d2e537aabc3062d4ce8fc4faf
jean-robin.peiteado <jean-robin.peiteado@cherrybiotech.com>
2025-12-05 13:44:36 +0100

    standalone with a tweak on the connection state while sending

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
+from __future__ import annotations
 import socket
+import time
 import asyncio
-from fastapi import FastAPI, WebSocket, Request, Response
+from fastapi import FastAPI, WebSocket, Request, Response, websockets
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
+HOST = "127.0.0.1"
+PORT = 5050
 
-from listen import receive_text, HOST, PORT
+def receive_text(conn:socket):
+    if not conn:
+        return
+    buffer = ""
+    while "\n" not in buffer:
+        data = conn.recv(1024).decode("utf-8")
+        if not data:
+            return buffer
+        buffer += data
+    return buffer.strip("\n")
 
+
+def listenSocket(address: tuple):
+    _, port = address
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind(address)
+            s.listen()
+            print(f"Listening on port {port}...")
+
+            while True:
+                time.sleep(0.5)
+                conn, addr = s.accept()
+                with conn:
+                    print(f"Connected by {addr}")
+                    while True:
+                        # result = unpickle_data(conn)
+                        result = receive_text(conn=conn)
+                        if result is None:
+                            conn.close()
+                            break
+                        print(result)
+        except KeyboardInterrupt as ke:
+            print(f"{repr(ke)}")
+            return
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 ADDRESS = (HOST,PORT)
 
@@ -23,7 +59,6 @@ async def websocket_endpoint(websocket: WebSocket,address=ADDRESS):
         s.bind(address)
         s.listen()
         print(f"Listening on port {port}...")
-
         while True:
             await asyncio.sleep(1.0)
             conn, addr = s.accept()
@@ -35,11 +70,8 @@ async def websocket_endpoint(websocket: WebSocket,address=ADDRESS):
                     if result is None:
                         conn.close()
                         break
-                    await websocket.send_text(result)
-
+                    if websocket.client_state == websockets.WebSocketState.CONNECTED:
+                        await websocket.send_text(result)
 @app.get("/")
 async def get_homepage(request: Request):
     return templates.TemplateResponse(request=request,context={},name="index.html")
-
-if __name__ == "__main__":
-    uvicorn.run(app,"localhost",8000)
